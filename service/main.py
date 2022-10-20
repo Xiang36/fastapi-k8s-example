9f3adc9a1c5bd6948b7edec4172b458b5d495ef9
devin <devin.wang@moneybook.com.tw>
2022-10-21 00:05:38 +0800

    first commit

---
+++ service/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from service.api.api_v1.api import router as api_router
+from service.core.config import API_V1_STR, PROJECT_NAME
+
+app = FastAPI(
+    title=PROJECT_NAME,
+)
+
+app.include_router(api_router, prefix=API_V1_STR)
+
+
+@app.get("/ping", summary="Check that the service is operational")
+def pong():
+    return {"ping": "pong!"}
